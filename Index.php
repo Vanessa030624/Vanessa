12a12a05446e9e6ccdea1cfdaff5b75922aab594
Vanessa030624 <ccccvvvv030624@gmail.com>
2024-09-18 02:35:03 -0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Personal de Vanessa García Vallejo</title>
+    <link rel="stylesheet" href="css/Index.css">
+</head>
+<body>
+
+    <!--
+        Fecha Modificación:     27 Junio 2024
+        Autor:                  Vanessa García Vallejo
+    -->
+    <center><h1>Index</h1></center>
+    <h2 id="web">Web</h2>
+        <center><table border="1" width="100%" id="table1">
+            <tr>
+                <td id="headd">No. Programa</td>
+                <td id="headd">Nombre de programa</td>
+                <td id="headd">Objetivo del programa</td>
+                <td id="headd">Fecha de creación</td>
+                <td id="headd">Fecha de modificacion</td>
+                <td id="headd">Parcial</td>
+                <td id="headd">Link</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Telefono</td>
+                <td>Declaracion de clase basica</td>
+                <td>30 de Agosto del 2024</td>
+                <td>04 de Septiembre del 2024</td>
+                <td>1</td>
+                <td><a href="PrimerParcial/Codigo1/Instancia.php"><img src="Img/icon3.gif" alt="telefonos" width="24px"></a></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Telefono 2</td>
+                <td>Declaracion de clase basica con diferentes funciones</td>
+                <td>05 de Septiembre del 2024</td>
+                <td>06 de Septiembre del 2024</td>
+                <td>1</td>
+                <td><a href="PrimerParcial/Codigo2/Instancia2.php"><img src="Img/icon3.gif" alt="telefonos" width="24px"></a></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Carro</td>
+                <td>Declaracion de clase basica con diferentes funciones</td>
+                <td>09 de Septiembre del 2024</td>
+                <td>11 de Septiembre del 2024</td>
+                <td>1</td>
+                <td><a href="PrimerParcial/Codigo3/Instancia.php"><img src="Img/icon3.gif" alt="carro" width="24px"></a></td>
+            </tr>
+            <tr>
+                <td>3.1</td>
+                <td>Auto</td>
+                <td>Declaracion de clase basica con diferentes funciones</td>
+                <td>09 de Septiembre del 2024</td>
+                <td>13 de Septiembre del 2024</td>
+                <td>1</td>
+                <td><a href="PrimerParcial/Codigo3/Instancia2.php"><img src="Img/icon3.gif" alt="auto" width="24px"></a></td>
+            </tr>
+            </table></center>
+    </body>
+</html>
